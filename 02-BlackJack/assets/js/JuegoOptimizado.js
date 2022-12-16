143ae28c5347400dd5a816b8550b6ac3b5b5e90d
KennethGranados <kennethgranados2001@gmail.com>
2022-12-15 20:01:42 -0600

    Clase 5

--- 02-BlackJack/assets/js/JuegoOptimizado.js
+++ 02-BlackJack/assets/js/JuegoOptimizado.js
@@ -1,11 +1,3 @@
-/**
- * DOM = Document Object Model (conjunto de objetos anidados para crear un documento y tener acceso
- * dinamico a través de programación y manipularlos)
- * 2C = Two of Clubs (Tréboles)
- * 2D = Two of Diamonds
- * 2H = Two of Hearts 
- * 2S = Two of Spades
-*/
 
 let deck       = [];
 let tipos      = ['C','D','H','S']
@@ -14,15 +6,14 @@ let especiales = ['A','J','K','Q']
 let puntosJugador     = 0,
     puntosComputadora = 0;
 
-//Referencias del HTML
 const btnPedir = document.querySelector('#btnPedir');
 const btnNuevo = document.querySelector('#btnNuevo');
 const btnDetener = document.querySelector('#btnDetener');
 
 const divCartasJugador     = document.querySelector('#jugador-cartas')
 const divCartasComputadora = document.querySelector('#computadora-cartas')
-// const smallJugador = document.querySelector('small'); solo selecione el primer smmall
-const puntosHTML = document.querySelectorAll('small'); //selecciona todos
+
+const puntosHTML = document.querySelectorAll('small'); 
 
 const crearDeck = () => {
  
@@ -40,7 +31,6 @@ const crearDeck = () => {
 
       }
 
-      //console.log( deck )
       deck = _.shuffle( deck );
       console.log( deck );
       return deck;
@@ -49,23 +39,18 @@ const crearDeck = () => {
 
 crearDeck();
 
-//Funcion que me permite tomar una carta
-
 const pedirCarta = () => {
 
    if ( deck.length ===0 ){
       throw 'No hay mas cartas en la baraja'
    }
 
-   const carta = deck.shift();// regresa el primer valor del arreglo
-   //const carta = deck.pop();// regresa el último valor del arreglo
-   // console.log( deck );
-   // console.log( carta );
+   const carta = deck.shift();
 
    return carta;
 }
 
-// turno d la computadora
+
 const turnoComputadora = ( puntosMinimos ) => {
    
    do {
@@ -74,9 +59,8 @@ const turnoComputadora = ( puntosMinimos ) => {
       puntosComputadora = puntosComputadora + valorCarta( carta );
       puntosHTML[1].innerText = puntosComputadora;
 
-      // <img class="carta" src="assets/cartas/2C.png"> 
       const imgCarta = document.createElement('img');
-      imgCarta.src = `assets/cartas/${ carta }.png` ; //asi es como se insertar codigo javascript
+      imgCarta.src = `assets/cartas/${ carta }.png` ;
       imgCarta.classList.add('carta');
       divCartasComputadora.append(imgCarta);
 
@@ -101,43 +85,25 @@ const turnoComputadora = ( puntosMinimos ) => {
 
 }
 
-// pedirCarta();
 const valorCarta = ( carta ) =>{
 
    const valor = carta.substring(0, carta.length -1);
-   //let puntos = 0;
-   //console.log( valor );
-   // if ( isNaN() ) {// is not at number(isNaN) //forma 1
-       //console.log('No es un número')
-   //    puntos = ( valor === 'A' ) ? 11 : 10; // Operador ternario si(?) se cumple la condición vale 11 si no(:) val 10
 
-   // } else {
-     //console.log('Si es un número')
-   //    puntos = valor * 1;
-   //console.log(puntos)
-   // }
-   //forma 2
-   return ( isNaN(valor) ) ? /**puntos = */( valor === 'A' ) ? 11 : 10
-                      : valor * 1;
+   return ( isNaN(valor) )  ? 
+          ( valor === 'A' ) ? 11 : 10
+                            : valor * 1;
    
 
 }
 
-//const valor = valorCarta( pedirCarta() )
-//console.log( {valor} )
-
-
-// Eventos
-//CallBack funcion que se manda como argumento
 btnPedir.addEventListener('click', () =>{
 
    const carta = pedirCarta();
    puntosJugador = puntosJugador + valorCarta( carta );
    puntosHTML[0].innerText = puntosJugador;
 
-   // <img class="carta" src="assets/cartas/2C.png"> 
    const imgCarta = document.createElement('img');
-   imgCarta.src = `assets/cartas/${ carta }.png` ; //asi es como se insertar codigo javascript
+   imgCarta.src = `assets/cartas/${ carta }.png` ; 
    imgCarta.classList.add('carta');
    divCartasJugador.append(imgCarta);
 
